26e2b9bda4c9463749189be67ee0c1aa7fb2ec38
bestGao <1055652678@qq.com>
2023-10-28 18:24:46 +0800

    feat(m): 修复错误的common配置

--- packages/common/vite.config.ts
+++ packages/common/vite.config.ts
@@ -5,6 +5,6 @@ import dts from 'vite-plugin-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { lib: { entry: resolve(__dirname, 'src/main.ts'), formats: ['es'] } },
-  resolve: { alias: { '@': resolve('src/') } },
+  resolve: { alias: { src: resolve('src/') } },
   plugins: [dts()],
 });
